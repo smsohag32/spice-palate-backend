bdb4f4410a1e699299db486cfa5087650b7ba0ef
SM Sohag <sohagsheik32@gmail.com>
2023-05-03 11:48:30 +0600

    data send in localserver done

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 4200;
+const cors = require('cors')
+const port = process.env.PORT || 4200;
+app.use(cors());
+
+const chefs = require('./data/chefsData.json');
+const recipes = require('./data/recipesData.json')
+
+app.get('/', (req,res)=>{
+    res.send(chefs)
+})
+app.get('/chefs', (req, res)=>{
+    res.send(chefs)
+})
+
+app.get('/chefs/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    const selectedChef = chefs.find(chef => parseInt(chef.id) === id);
+    res.send(selectedChef);
+})
+app.get('/recipes', (req,res)=> {
+    res.send(recipes)
+})
+app.get('/recipes/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const chefRecipes = recipes.filter(recipe => parseInt(recipe.chefId) === id)
+    res.send(chefRecipes)
+})
+
+app.listen(port)
